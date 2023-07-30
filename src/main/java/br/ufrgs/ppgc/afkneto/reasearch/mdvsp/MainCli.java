468c13a2273a1efff6fd8ae29a45c60a1281de9c
alberto <alberto@inf.ufsm.br>
2023-07-30 16:15:31 -0300

    Add initial code and project structure

---
+++ src/main/java/br/ufrgs/ppgc/afkneto/reasearch/mdvsp/MainCli.java
@@ -0,0 +1,13 @@
+package br.ufrgs.ppgc.afkneto.reasearch.mdvsp;
+
+/**
+ * Hello world!
+ *
+ */
+public class MainCli  {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
